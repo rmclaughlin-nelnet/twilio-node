f47a5c3016387792ff79af8e224ee39211358e1c
Ryan McLaughlin <ryan.mclaughlin@nelnetbank.com>
2026-03-06 08:55:07 -0700

    refactor: remove root path from Vitest cluster configuration
    - Eliminated the root path setting from the Vitest configuration for cluster tests, streamlining the test setup.
    - This change enhances flexibility in test file organization and execution.

--- vitest.cluster.config.ts
+++ vitest.cluster.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    root: "./spec/cluster",
     include: ["*.spec.{js,ts}"],
     testTimeout: 30000,
   },
